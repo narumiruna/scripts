d8cac83c1912292ebf9f17110f38e29e062bb6b7
なるみ <weaper@gmail.com>
2019-05-04 06:08:03 +0800

    Create install_zsh.sh

---
+++ install_zsh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y curl git zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/narumiruna/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+env zsh -l
