8c16cd28eb49ef54c208857f3bb270b7fa7185bd
なるみ <weaper@gmail.com>
2019-05-04 06:11:37 +0800

    Update install_pyenv.sh

--- install_pyenv.sh
+++ install_pyenv.sh
@@ -5,3 +5,5 @@ echo '# pyenv' >> ~/.zshrc
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+
+source ~/.zshrc
