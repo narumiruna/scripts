0b0b60a477beb5dadb9236e21710495874eb944f
なるみ <weaper@gmail.com>
2019-05-04 14:22:50 +0800

    Update install_pyenv.sh

--- install_pyenv.sh
+++ install_pyenv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+git clone https://github.com/pyenv/pyenv.git ${HOME}/.pyenv
 
-echo '# pyenv' >> ~/.zshrc
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-echo 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+echo '# pyenv' >> ${HOME}/.zshrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ${HOME}/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ${HOME}/.zshrc
+echo 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ${HOME}/.zshrc
 
-source ~/.zshrc
+source ${HOME}/.zshrc
