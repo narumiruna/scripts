3b12876938c10256729cb5e08b36791b6844f7d9
なるみ <weaper@gmail.com>
2019-05-04 14:04:36 +0800

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -9,4 +9,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+curl https://github.com/narumiruna/scripts/blob/master/zshrc --output ${HOME}/.zshrc
+
 env zsh -l
