867da3468ed8463d6f0c84296dd79f47314a982d
なるみ <weaper@gmail.com>
2019-05-14 23:48:10 +0800

    Update install_pyenv.sh

--- install_pyenv.sh
+++ install_pyenv.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# https://github.com/pyenv/pyenv/wiki
+sudo apt-get update
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
 git clone https://github.com/pyenv/pyenv.git ${HOME}/.pyenv
 
 echo '# pyenv' >> ${HOME}/.zshrc
