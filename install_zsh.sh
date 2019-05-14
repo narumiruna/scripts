c64b4d2aa70027d153e36a87627c11d11877cdb2
なるみ <weaper@gmail.com>
2019-05-14 23:32:23 +0800

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -10,6 +10,6 @@ git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugi
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-curl https://github.com/narumiruna/scripts/blob/master/zshrc --output ${HOME}/.zshrc
+curl https://raw.githubusercontent.com/narumiruna/scripts/master/zshrc --output ${HOME}/.zshrc
 
 env zsh -l
