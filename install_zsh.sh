6b91f90c698bbee53040cec6f9593448ef619e97
なるみ <weaper@gmail.com>
2019-09-09 21:09:25 +0800

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -8,7 +8,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/narumiruna/oh-my-zsh/maste
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
 curl https://raw.githubusercontent.com/narumiruna/scripts/master/zshrc --output ${HOME}/.zshrc
 
